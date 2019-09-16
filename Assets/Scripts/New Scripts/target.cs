3ea25ee7c50fde458ce73f646d87928b60d58d2f
jaredrothwell <31265730+jaredrothwell@users.noreply.github.com>
2019-09-16 15:46:46 -0600

    level_3 using the bow
    This will actually be level 2

---
+++ Assets/Scripts/New Scripts/target.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class target : MonoBehaviour
+{
+    public GameObject floor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        floor.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "arrow")
+        {
+            floor.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
