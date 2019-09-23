6b7c2300f08ff34c5d11d3d387ed26d9e5fc9542
jaredrothwell <31265730+jaredrothwell@users.noreply.github.com>
2019-09-23 16:31:21 -0600

    Additional puzzle added

---
+++ Assets/Scripts/New Scripts/NewDoor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewDoor : MonoBehaviour
+{
+    public GameObject trigger1;
+    public GameObject trigger2;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(trigger1.GetComponent<trigger>().player != null && trigger2.GetComponent<trigger>().player != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
